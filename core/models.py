babfdca6a27ab757785e3397fc7aa5258f381c0e
scarter13 <scarter13@gmail.com>
2020-06-17 12:05:17 -0400

    cleaned up unnecessary spaces in the models

--- core/models.py
+++ core/models.py
@@ -4,19 +4,19 @@ from users.models import User
 
 # Create your models here.
 class Question(models.Model):
-    user = models.ForeignKey(to = User, on_delete = models.CASCADE, null=True, related_name = 'questions')
-    headline = models.CharField(max_length = 255)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, related_name='questions')
+    headline = models.CharField(max_length=255)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 
 class Answer(models.Model):
-    author = models.ForeignKey(to = User, on_delete = models.CASCADE, null=True, related_name = 'answers')
-    question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = 'answers')
+    author = models.ForeignKey(to=User, on_delete=models.CASCADE, null=True, related_name='answers')
+    question = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name='answers')
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
 
 class StarQuestion(models.Model):
-    user = models.ForeignKey(to = Question, on_delete = models.CASCADE, related_name = 'star_questions')
+    user = models.ForeignKey(to=Question, on_delete=models.CASCADE, related_name='star_questions')
     
 
